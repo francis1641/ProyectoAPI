ea0f8493967281010d5bb57c8b7e7cc4bbb912f9
francis1641 <73337211+francis1641@users.noreply.github.com>
2021-05-02 17:05:35 +0200

    Update index

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function loadDoc() {
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         misDatos= JSON.parse(this.responseText);
-        mostrarTitulo(misDatos);
+        //mostrarTitulo(misDatos);
+        crearCard(misDatos);
       }
     };
     xhttp.open("GET", "https://newsapi.org/v2/top-headlines?country=us&category=entertainment&everything?q=Apple&from=2021-05-01&sortBy=popularity&apiKey=5e976ef6430f49e08d17ea33c7605f41", true);
@@ -28,10 +29,37 @@ function crearCard(datos){
   divimg=document.createElement("div");
   divimg.setAttribute("class", "noticias__card--img");
   img=document.createElement("img");
-  img.setAttribute("src", datos.articles[TODAVIA NO SABEMOS QUE PONER].urlToImage);
+  img.setAttribute("src", datos.articles[1].urlToImage);
+  titulo=document.createElement("h3");
+  titulo.innerHTML= datos.articles[1].title;
+
 
   divcontent=document.createElement("div");
   divcontent.setAttribute("class", "noticias__card--content");
+  autor=document.createElement("p");
+  autor.innerHTML=datos.articles[1].author;
+  contenido=document.createElement("p");
+  contenido.innerHTML=datos.articles[1].content;
+  descripcion=document.createElement("p");
+  descripcion.innerHTML=datos.articles[1].description;
+  publicado=document.createElement("p");
+  publicado.innerHTML=datos.articles[1].publishedAt;
+  url=document.createElement("p");
+  url.innerHTML=datos.articles[1].url;
+
+  divimg.appendChild(img);
+  divimg.appendChild(titulo);
+  
+  divcontent.appendChild(autor);
+  divcontent.appendChild(contenido);
+  divcontent.appendChild(descripcion);
+  divcontent.appendChild(publicado);
+  divcontent.appendChild(url);
+
+  divpadre.appendChild(divimg);
+  divpadre.appendChild(divcontent);
+
+  document.body.appendChild(divpadre);
 
 }
 
